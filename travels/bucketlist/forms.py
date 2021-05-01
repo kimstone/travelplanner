cf9411e4d9f03990a992a3baa2ab1ac0b3006d20
Kim Stone <techmate@Mac-mini.localdomain>
2021-05-01 11:27:17 -0400

    removed outdated file

--- travels/bucketlist/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.forms import ModelForm
-from .models import *
-
-class BucketListForm(forms.ModelForm):
-
-    class Meta:
-        model = BucketList
-        fields = '__all__'
